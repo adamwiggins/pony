16855ce830b588083596b85c0caa190dca2ec5af
Adam Wiggins <adam@heroku.com>
2008-09-23 00:33:02 -0700

    mail_via_smtp with full options hash

--- lib/pony.rb
+++ lib/pony.rb
@@ -2,8 +2,12 @@ require 'net/smtp'
 
 module Pony
 	def self.mail(to, body)
+		mail_via_smtp(:to => to, :body => body)
+	end
+
+	def self.mail_via_smtp(options={})
 		Net::SMTP.start('localhost') do |smtp|
-			smtp.sendmail(body, 'pony@unknown', [ to ])
+			smtp.sendmail(options[:body], options[:from] || 'pony@unknown', [ options[:to] ])
 		end
 	end
 end
--- spec/pony_spec.rb
+++ spec/pony_spec.rb
@@ -1,10 +1,15 @@
 require File.dirname(__FILE__) + '/base'
 
 describe Pony do
-	it "sends an email via Net::SMTP" do
+	it "transports mail via Net::SMTP connecting to localhost" do
 		smtp = mock('net::smtp object')
 		Net::SMTP.should_receive(:start).with('localhost').and_yield(smtp)
-		smtp.should_receive(:sendmail).with('Hello, Joe!', 'pony@unknown', [ 'joe@example.com' ])
+		smtp.should_receive(:sendmail).with('Hello, Joe!', 'spec@pony', [ 'joe@example.com' ])
+		Pony.mail_via_smtp(:to => 'joe@example.com', :from => 'spec@pony', :body => "Hello, Joe!")
+	end
+
+	it "simplest possible syntax: mail(to, body)" do
+		Pony.should_receive(:mail_via_smtp).with(:to => 'joe@example.com', :body => 'Hello, Joe!')
 		Pony.mail('joe@example.com', 'Hello, Joe!')
 	end
 end
