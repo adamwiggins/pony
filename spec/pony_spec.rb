f48635aa3b03ee330bf735058f4e9b99afda5889
arunthampi <arun.thampi@gmail.com>
2008-11-06 11:03:21 +0800

    Add :via option which overrides the default transport mechanism

--- spec/pony_spec.rb
+++ spec/pony_spec.rb
@@ -72,4 +72,21 @@ describe Pony do
 			Pony.transport_via_smtp(mock('tmail', :to => 'to', :from => 'from', :to_s => 'message'))
 		end
 	end
+	
+  describe ":via option should over-ride the default transport mechanism" do
+    it "should send via sendmail if :via => sendmail" do
+      Pony.should_receive(:transport_via_sendmail)
+  		Pony.mail(:to => 'joe@example.com', :via => :sendmail)
+		end
+
+    it "should send via smtp if :via => smtp" do
+      Pony.should_receive(:transport_via_smtp)
+  		Pony.mail(:to => 'joe@example.com', :via => :smtp)
+		end
+		
+		it "should raise an error if via is neither smtp nor sendmail" do
+		  lambda { Pony.mail(:to => 'joe@plumber.com', :via => :pigeon) }.should raise_error(ArgumentError)
+	  end
+	end
+	
 end
